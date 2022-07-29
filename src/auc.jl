9175a03b7dfafcd9f62d209888f9763226da22c0
hayesall <alexander@batflyer.net>
2022-07-29 11:58:08 -0400

    💡 Add comments based on those in original source

--- src/auc.jl
+++ src/auc.jl
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
-Adapted from Matlab code by Dr. Gavin C. Cawley.
+Adapted from MATLAB code by Dr. Gavin C. Cawley.
 
 http://theoval.cmp.uea.ac.uk/matlab/#roc
 
@@ -27,6 +27,30 @@ function A = auroc(tp, fp)
     A = sum((fp(2:n) - fp(1:n-1)).*(tp(2:n)+tp(1:n-1)))/2;
 endfunction
 ```
+
+The comments in the original MATLAB source, this was developed
+or released around 2001-03-22:
+
+- 2001-03-22: v1.00
+- 2004-11-10: v1.01 - minor improvements to comments etc.
+
+Cawley's motivation included the description:
+
+> An ROC (receiver operator characteristic) curve is a plot of the true
+> positive rate as a function of the false positive rate of a classifier
+> system. The area under the ROC curve is a reasonable performance
+> statistic for classifier systems assuming no knowledge of the true ratio
+> of misclassification costs.
+>
+> A = AUROC(TP, FP) computes the area under the ROC curve, where TP and FP
+> are column vectors defining the ROC or ROCCH curve of a classifier system.
+
+References
+----------
+
+[1] Fawcett, T., "ROC graphs: Notes and practical considerations for
+    researchers", Technical report, HP Laboratories, MS 1143, 1501
+    Page Mill Road, Palo Alto CA 94304, USA, April 2004.
 """
 
 module CawleyAUC
